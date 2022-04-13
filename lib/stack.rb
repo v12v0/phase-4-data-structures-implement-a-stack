db1d9ea292a21209f1c0a1add4e0eb340e579f9d
Vi Vo <aaronvo118@gmail.com>
2022-04-13 10:00:42 -0500

    commit tree 6de5af8be6628d07ba2de451f71115cb15f31a70

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
 # your code here
+class Stack 
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        @stack.push(value)
+    end
+
+    def pop 
+        @stack.pop
+    end
+
+    def peek 
+        @stack.last
+    end
+end
